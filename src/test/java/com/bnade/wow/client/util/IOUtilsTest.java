2f41630f5be760ec120eb67c35f849bd36a7e8a7
Feng Liu <liufeng0103@163.com>
2017-06-30 17:08:31 +0800

    修复IOUtils测试case

--- src/test/java/com/bnade/wow/client/util/IOUtilsTest.java
+++ src/test/java/com/bnade/wow/client/util/IOUtilsTest.java
@@ -3,6 +3,7 @@ package com.bnade.wow.client.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 /**
@@ -12,7 +13,7 @@ public class IOUtilsTest {
 
     @Test
     public void inputStreamToStringTest() throws Exception {
-        InputStream is = null;
+        InputStream is = new ByteArrayInputStream("测试".getBytes("utf-8"));
         Assert.assertEquals("测试", IOUtils.inputStreamToString(is, "utf-8"));
     }
 
